658a0b0bf2f6a40fe990d4ce557feaa80144cf16
Rafael Willians <me@rwillians.com>
2025-08-23 14:31:00 -0300

    Updated bookk

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule BookkEcto.MixProject do
 
   defp deps do
     [
-      {:bookk, github: "rwillians/bookk", ref: "3ea2aa5"},
+      {:bookk, github: "rwillians/bookk", ref: "77d26c7"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false, optional: true},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false, optional: true},
       {:ecto, "~> 3.13"},
